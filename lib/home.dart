aa831f3fa84dccaa252d3f049af3cd2139385948
dell <abdelrhmaninsta1@gmail.com>
2025-09-13 22:55:20 +0300

    app init

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,8 +9,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ToDo",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Colors.white),),
+        title: Text("ToDo",style: TextStyle(fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
+        toolbarHeight:100,
+      ),
+      body: Column(
+        children: [
+          CalendarTimeline(
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now().subtract(Duration(days: 356)),
+            lastDate: DateTime.now().add(Duration(days: 356)),
+            onDateSelected: (date) => print(date),
+            leftMargin: 20,
+            monthColor: Colors.blueGrey,
+            dayColor: Colors.teal[200],
+            activeDayColor: Colors.white,
+            activeBackgroundDayColor: Colors.redAccent[100],
+            selectableDayPredicate: (date) => date.day != 23,
+            locale: 'en_ISO',
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routename,
+      initialRoute: Home.routename,
       routes: {
         Home.routename: (context) => const Home(),
         SplashScreen.routename: (context) => const SplashScreen(),
