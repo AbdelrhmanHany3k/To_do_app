6a30a72efdbebb8e0bfbbc076c8347e76989a201
dell <abdelrhmaninsta1@gmail.com>
2025-09-19 23:22:00 +0300

    shared prefrences added

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.light;
 
-  void changeTheme(String mode) {
+  ThemeProvider() {
+    loadTheme(); // أول ما يشتغل يجيب آخر اختيار
+  }
+
+  void changeTheme(String mode) async {
     if (mode == "Light") {
       themeMode = ThemeMode.light;
     } else {
       themeMode = ThemeMode.dark;
     }
     notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("themeMode", mode); // حفظ الاختيار
+  }
+
+  Future<void> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? savedMode = prefs.getString("themeMode");
+
+    if (savedMode != null) {
+      if (savedMode == "Light") {
+        themeMode = ThemeMode.light;
+      } else {
+        themeMode = ThemeMode.dark;
+      }
+      notifyListeners();
+    }
   }
 }
